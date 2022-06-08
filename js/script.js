7569d838fa099c6bc0eee744f1b16a64793c5ced
Rafiqul Islam <106866101+rafiqulislamrt@users.noreply.github.com>
2022-06-08 16:52:54 +0300

    Swiper Slider

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+    var swiper = new Swiper('.swiper-container', {
+            effect: 'cube',
+        grabCursor: true,
+        cubeEffect: {
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 20,
+        shadowScale: 0.94,
+    },
+          autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+      },speed: 1000,
+          pagination: {
+                el: '.swiper-pagination',
+                type: 'fraction',
+              },
+          navigation: {
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
+            },
+
+    })
+
+
+});
